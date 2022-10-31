d607bd633cc800041e663829fb4f9620c20db8f4
zhongjinglin <zjl_java@163.com>
2022-11-01 01:06:19 +0800

    feat: 手写call、apply、bind

---
+++ 手写call、apply、bind/index.js
@@ -0,0 +1,31 @@
+Function.prototype._call = function(thisArg, ...args) {
+  const fn = this
+  thisArg = thisArg === null || thisArg === undefined ? window : Object(thisArg)
+  thisArg.fn = fn
+  return thisArg.fn(...args)
+}
+
+Function.prototype._apply = function(thisArg, argArray = []) {
+  const fn = this
+  thisArg = thisArg === null || thisArg === undefined ? window : Object(thisArg)
+  thisArg.fn = fn
+  return thisArg.fn(...argArray)
+}
+
+Function.prototype._bind = function (thisArg, ...args) {
+  const fn = this
+  const tempFn = Symbol()
+  thisArg = thisArg === null || thisArg === undefined ? window : Object(thisArg)
+  function newFn(...params) {
+    if (this.__proto__.constructor === newFn) {
+      this[tempFn] = fn
+      this[tempFn](...params)
+      delete this[tempFn]
+    } else {
+      thisArg[tempFn] = fn
+      thisArg[tempFn](...args.concat(params))
+      delete thisArg[tempFn]
+    }
+  }
+  return newFn
+}
